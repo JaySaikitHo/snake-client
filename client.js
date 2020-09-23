c767e5140ad36e0311d0118f0ebd555efa06ebbc
vagrant <ttjho@hotmail.com>
2020-09-23 21:59:17 +0000

    added up movement

--- client.js
+++ client.js
@@ -1,27 +1,35 @@
-const net = require('net')
+const net = require('net');
 
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-      
-  conn.on('connect', () => {
-    console.log("successfully connected to game server")
-    
-      conn.write('Name: Jay')
-      
-  });
-  
-  conn.on('data', (data) => {
-    console.log('Message from client: ', data)
-    });
-  
-  
-  return conn;
-}
+  conn.setEncoding('utf8');
 
-module.exports = {connect}
+  conn.on('connect', () => {
+    console.log("successfully connected to game server");
+    conn.write('Name: Jay');
+    setInterval(() => {conn.write("Move: up")}, 5000);
+    setInterval(() => {conn.write("Move: up")}, 5000);
+    setInterval(() => {conn.write("Move: right")}, 5000);
+    setInterval(() => {conn.write("Move: right")}, 5000);
+
+    conn.on('data', (data) => {
+      console.log('Message from client: ', data);
+    });
+  });
+
+
+
+
+
+
+
+  return conn;
+};
+
+
+module.exports = { connect };
